c46fb79a391f07b7c009a58dde7b3238c57f6c2d
Indiel <kori.yanagi@gmail.com>
2018-02-04 22:13:40 +0200

    Исправлено отображение комментариев

--- js/pictures.js
+++ js/pictures.js
@@ -19,7 +19,7 @@ var arrFilling = function (arr) {
 };
 
 // функция для рандомного выведения значений массива без повторений
-var getUrlPhotos = function (arr) {
+var shuffle = function (arr) {
   var randomValue = Math.floor(Math.random() * arr.length);
   return arr.splice(randomValue, 1).toString();
 };
@@ -30,10 +30,19 @@ var getRandomNumber = function (min, max) {
 
 arrFilling(urls);
 
+// заполнение свойств фотографий
 function Photos(arrUrls, arrComments) {
-  this.url = 'photos/' + getUrlPhotos(arrUrls) + '.jpg';
+  this.url = 'photos/' + shuffle(arrUrls) + '.jpg';
   this.likes = getRandomNumber(15, 200);
-  var arrComm = [getUrlPhotos(arrComments), getUrlPhotos(arrComments)];
+  var arrComm = [];
+  var rand = getRandomNumber(1, 5);
+  var rand2 = getRandomNumber(1, 3);
+  for (var i = 0; i < rand; i++) {
+    arrComm.push(shuffle(arrComments));
+    for (var j = 0; j < rand2; j++) {
+      arrComm[i] += shuffle(arrComments);
+    }
+  }
   this.comments = arrComm;
 }
 
@@ -41,7 +50,7 @@ var pictureTemplate = document.querySelector('#picture-template').content;
 var picturesListElement = document.querySelector('.pictures');
 var pictureFragment = document.createDocumentFragment();
 
-// заполнение контейнера шаблонными фрагментами
+// заполнение контейнера шаблонными фрагментами (фотографиями)
 var createPictureElement = function (photo) {
   var photoElement = pictureTemplate.cloneNode(true);
 
@@ -52,6 +61,7 @@ var createPictureElement = function (photo) {
   return pictureFragment.appendChild(photoElement);
 };
 
+// отрисовываем все фотографии на странице
 for (var i = 0; i < urls.length; i++) {
   doubleComments = COMMENTS.slice();
   photos[i] = new Photos(urls, doubleComments);
@@ -63,10 +73,28 @@ picturesListElement.appendChild(pictureFragment);
 var galleryOverlay = document.querySelector('.gallery-overlay');
 galleryOverlay.classList.remove('hidden');
 
+// склонение слова
+var getDeclension = function (arrLength) {
+  var commStr;
+  switch (arrLength % 10) {
+    case 1: commStr = ' комментарий';
+      break;
+    case 2:
+    case 3:
+    case 4: commStr = ' комментария';
+      break;
+    case 5: commStr = ' комментариев';
+      break;
+  }
+  return commStr;
+};
+
+// отрисовка окна открытой фотографии
 var createOverlayElement = function (photo) {
   galleryOverlay.querySelector('.gallery-overlay-image').setAttribute('src', photo.url);
   galleryOverlay.querySelector('.likes-count').textContent = photo.likes;
-  galleryOverlay.querySelector('.comments-count').textContent = photo.comments.length;
+  // galleryOverlay.querySelector('.comments-count').textContent = photo.comments.length;
+  galleryOverlay.querySelector('.gallery-overlay-controls-comments').textContent = photo.comments.length + getDeclension(photo.comments.length);
 
   return galleryOverlay;
 };
