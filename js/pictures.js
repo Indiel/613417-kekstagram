ec81561cd056606332c767f61d7c0e6e1d223e22
Indiel <kori.yanagi@gmail.com>
2018-02-11 16:04:29 +0200

    Попытка реализовать сценарий загрузки изображения и его редактирования

--- js/pictures.js
+++ js/pictures.js
@@ -44,16 +44,6 @@
     this.comments = arrComm;
   };
 
-    // заполнение контейнера шаблонными фрагментами (фотографиями)
-  var createPictureElement = function (photo) {
-    var photoElement = pictureTemplate.cloneNode(true);
-
-    photoElement.querySelector('img').setAttribute('src', photo.url);
-    photoElement.querySelector('.picture-likes').textContent = photo.likes;
-    photoElement.querySelector('.picture-comments').textContent = photo.comments.length;
-    pictureFragment.appendChild(photoElement);
-  };
-
   // склонение слова
   var getDeclension = function (arrLength) {
     var commStr;
@@ -79,7 +69,19 @@
     return commStr;
   };
 
+    // заполнение контейнера шаблонными фрагментами (фотографиями)
+  var createPictureElement = function (photo) {
+    var photoElement = pictureTemplate.cloneNode(true);
+
+    photoElement.querySelector('img').setAttribute('src', photo.url);
+    photoElement.querySelector('.picture-likes').textContent = photo.likes;
+    photoElement.querySelector('.picture-comments').textContent = photo.comments.length;
+    pictureFragment.appendChild(photoElement);
+  };
+
   // отрисовка окна открытой фотографии
+  var galleryOverlay = document.querySelector('.gallery-overlay');
+
   var createOverlayElement = function (photo) {
     galleryOverlay.querySelector('.gallery-overlay-image').setAttribute('src', photo.url);
     galleryOverlay.querySelector('.likes-count').textContent = photo.likes;
@@ -98,12 +100,145 @@
     doubleComments = COMMENTS.slice();
     photos[i] = new Photos(urls, doubleComments);
     createPictureElement(photos[i]);
+
+    // (function (photos[i]) {
+    //   pictureFragment.addEventListener('click', function () {
+    //     createOverlayElement(photos[i]);
+    //     openPopup(galleryOverlay);
+    //   });
+    // })();
+
+    // photos[i].addEventListener('click', function () {
+    //   createOverlayElement(photos[i]);
+    //   openPopup(galleryOverlay);
+    // });
+
+    // (function (i) {
+    //   picturesListElement.addEventListener('click', function (evt) {
+    //     if (evt.target.tagName === 'IMG') {
+    //       createOverlayElement(photos[i]);
+    //       openPopup(galleryOverlay);
+    //     }
+    //   });
+    // })();
   }
 
   picturesListElement.appendChild(pictureFragment);
 
-  var galleryOverlay = document.querySelector('.gallery-overlay');
-  galleryOverlay.classList.remove('hidden');
+  // Показ изображения в полноэкранном режиме
 
-  createOverlayElement(photos[0]);
+  // galleryOverlay.classList.remove('hidden');
+  // createOverlayElement(photos[0]);
+
+
+  // picturesListElement.addEventListener('click', function (evt) {
+  //   if (evt.target.tagName === 'IMG') {
+  //     openPopup(galleryOverlay);
+  //     createOverlayElement(photos[0]);
+  //   }
+  // });
+
+  // Показ формы редактирования
+  var ENTER_KEYCODE = 13;
+  var ESC_KEYCODE = 27;
+
+  var uploadFile = document.querySelector('#upload-file');
+  var uploadOverlay = document.querySelector('.upload-overlay');
+  var overlayClose = document.querySelector('#upload-cancel');
+
+  var onPopupEscPress = function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      if (evt.target.tagName !== 'TEXTAREA') {
+        closePopup(uploadOverlay);
+      }
+    }
+  };
+
+  var openPopup = function (element) {
+    element.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+  };
+
+  var closePopup = function (element) {
+    element.classList.add('hidden');
+    document.removeEventListener('keydown', onPopupEscPress);
+    uploadFile.value = '';
+    if (imagePreview.classList.length > 1) {
+      imagePreview.classList.remove(imagePreview.classList[1]);
+    }
+    radios[0].checked = true;
+  };
+
+  uploadFile.addEventListener('change', function () {
+    openPopup(uploadOverlay);
+    imagePreview.style.transform = 'scale(1)';
+    resizeControls.value = '100%';
+    resizeButtonInc.classList.add('hidden');
+  });
+
+  overlayClose.addEventListener('click', function () {
+    closePopup(uploadOverlay);
+  });
+
+  overlayClose.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      closePopup(uploadOverlay);
+    }
+  });
+
+  // Применение эффекта для изображения
+  var imagePreview = document.querySelector('.effect-image-preview');
+  var radios = document.querySelectorAll('[name = "effect"]');
+
+  for (i = 0; i < radios.length; i++) {
+    radios[i].addEventListener('click', function () {
+      if (imagePreview.classList.length > 1) {
+        imagePreview.classList.remove(imagePreview.classList[1]);
+      }
+      var classEffect = this.id.replace('upload-', '');
+      imagePreview.classList.add(classEffect);
+    });
+  }
+
+  // var effectPin = document.querySelector('.upload-effect-level-pin');
+
+  // effectPin.addEventListener('mouseup', function () {
+  //   // effectPin.
+  // });
+
+  // Редактирование размера изображения
+  var resizeControls = document.querySelector('.upload-resize-controls-value');
+  var resizeButtonDec = document.querySelector('.upload-resize-controls-button-dec');
+  var resizeButtonInc = document.querySelector('.upload-resize-controls-button-inc');
+
+  var getScale = function () {
+    var scale = imagePreview.style.transform;
+    scale = Number(scale.slice(6, scale.length - 1));
+
+    return scale;
+  };
+
+  resizeButtonDec.addEventListener('click', function () {
+    var scale = getScale();
+    if (scale > 0.25) {
+      scale -= 0.25;
+      imagePreview.style.transform = 'scale(' + scale + ')';
+      resizeControls.value = scale * 100 + '%';
+      resizeButtonInc.classList.remove('hidden');
+    } if (scale === 0.25) {
+      resizeButtonDec.classList.add('hidden');
+    }
+  });
+
+  resizeButtonInc.addEventListener('click', function () {
+    var scale = getScale();
+    if (scale < 1) {
+      scale += 0.25;
+      imagePreview.style.transform = 'scale(' + scale + ')';
+      resizeControls.value = scale * 100 + '%';
+      resizeButtonDec.classList.remove('hidden');
+    } if (scale === 1) {
+      resizeButtonInc.classList.add('hidden');
+    }
+  });
 })();
